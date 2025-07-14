0603fe16a526852ee71dfb24ee9e4f91bc5a8c1c
Islom Murodov <brilliantpul@gmail.com>
2025-07-14 23:36:32 +0500

    init

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -32,8 +32,6 @@ async function registerUser() {
 
       isLoading.value = false
 
-      toast.success('Helloo')
-
       router.push('/vhod')
     }
     else {
@@ -70,9 +68,12 @@ async function registerUser() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
                   <path
                     d="M12.75 22.5C18.25 22.5 22.75 18 22.75 12.5C22.75 7 18.25 2.5 12.75 2.5C7.25 2.5 2.75 7 2.75 12.5C2.75 18 7.25 22.5 12.75 22.5Z"
-                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M8.5 12.5L11.33 15.33L17 9.67004" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round" />
+                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                  />
+                  <path
+                    d="M8.5 12.5L11.33 15.33L17 9.67004" stroke="white" stroke-width="1.5" stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
                 </svg>
                 Почта
               </div>
@@ -83,9 +84,12 @@ async function registerUser() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
                   <path
                     d="M12.75 22.5C18.25 22.5 22.75 18 22.75 12.5C22.75 7 18.25 2.5 12.75 2.5C7.25 2.5 2.75 7 2.75 12.5C2.75 18 7.25 22.5 12.75 22.5Z"
-                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M8.5 12.5L11.33 15.33L17 9.67004" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round" />
+                    stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                  />
+                  <path
+                    d="M8.5 12.5L11.33 15.33L17 9.67004" stroke="white" stroke-width="1.5" stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
                 </svg>
                 Номер телефона
               </div>
@@ -97,7 +101,7 @@ async function registerUser() {
           <button class="btn-register" type="submit" :disabled="isLoading" @click="() => registerUser()">
             Регистрация
             <div v-if="isLoading" style="margin-left: 10px;">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" /></path></svg>
             </div>
           </button>
           <RouterLink to="/register">
